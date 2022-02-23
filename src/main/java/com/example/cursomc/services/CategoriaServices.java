eec3c9af8e335b3fbf85fb8ec032d7650f27e0f9
Leonardo Rocha <leonardoleo182@hotmail.com>
2022-02-23 11:18:13 -0300

    Criando Repository e Service para categoria

---
+++ src/main/java/com/example/cursomc/services/CategoriaServices.java
@@ -0,0 +1,23 @@
+package com.example.cursomc.services;
+
+import java.util.Optional;
+import java.util.List;
+
+import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.repositories.CategoriaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaServices {
+    
+    @Autowired
+    private CategoriaRepository repo;
+    
+    public Categoria find(Integer id){
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+
+}
