f91023c2bf62a7d912f75e52a9cf13ee2fa2fa0d
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-02-26 18:23:46 -0300

    ItemPedido e ItemPedidoPk

--- src/main/java/com/example/cursomc/domain/Pedido.java
+++ src/main/java/com/example/cursomc/domain/Pedido.java
@@ -1,7 +1,8 @@
 package com.example.cursomc.domain;
 
 import java.util.Date;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -9,14 +10,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import java.io.Serializable;
-import java.util.ArrayList;
 
 @Entity
 public class Pedido implements Serializable{
@@ -38,12 +36,12 @@ public class Pedido implements Serializable{
     @ManyToOne
     @JoinColumn(name = "endereco_de_entrega_id")
     private Endereco enderecoDeEntrega;
-    
-    @JsonManagedReference
-	@ManyToMany(mappedBy="categorias")
-    private List<Produto> produtos = new ArrayList<>();
+
+    @OneToMany(mappedBy = "id.pedido")
+    private Set<ItemPedido> itens = new HashSet<>();
 
     public Pedido(){}
+
 
     public Pedido(Integer id, Date instante,Cliente cliente,Endereco enderecoDeEntrega){
         super();
@@ -93,12 +91,12 @@ public class Pedido implements Serializable{
         this.enderecoDeEntrega = enderecodeEntrega;
     }
 
-    public List<Produto> getProdutos() {
-        return produtos;
+    public Set<ItemPedido> getItens() {
+        return itens;
     }
 
-    public void setProdutos(List<Produto> produtos) {
-        this.produtos = produtos;
+    public void setItens(Set<ItemPedido> itens) {
+        this.itens = itens;
     }
 
     @Override
