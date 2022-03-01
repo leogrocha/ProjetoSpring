0c9d1784b4acc5b71c4b2fdb89f29dbdd06e4e68
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-03-01 04:01:24 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/example/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.cursomc.resources.exceptions;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.example.cursomc.services.exceptions.DataIntegrityException;
 import com.example.cursomc.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.http.HttpStatus;
@@ -18,4 +19,11 @@ public class ResourceExceptionHandler {
         e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
+
+    @ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandarError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+		
+		StandarError err = new StandarError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
 }
--- src/main/java/com/example/cursomc/services/CategoriaServices.java
+++ src/main/java/com/example/cursomc/services/CategoriaServices.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 import com.example.cursomc.domain.Categoria;
 import com.example.cursomc.repositories.CategoriaRepository;
 import com.example.cursomc.services.exceptions.ObjectNotFoundException;
+import com.example.cursomc.services.exceptions.DataIntegrityException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +16,7 @@ public class CategoriaServices {
     
     @Autowired
     private CategoriaRepository repo;
-    
+
     public Categoria find(Integer id){
         Optional<Categoria> obj = repo.findById(id);
 
@@ -31,6 +33,15 @@ public class CategoriaServices {
     public Categoria update(Categoria obj){
         find(obj.getId()); // buscando objeto no banco e caso não exista é chamado a exceção criada no método find.
         return repo.save(obj);
+    }
+
+    public void delete(Integer id){
+        find(id); // Verifica se existe o id e caso não existe gera a exceção do método
+        try {
+            repo.deleteById(id);
+        } catch(DataIntegrityViolationException e){
+            throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+        }
     }
 
 }
