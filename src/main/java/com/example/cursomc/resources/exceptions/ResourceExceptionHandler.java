286bd36c9e5e9222f5cc323c2f501f8f0d8c49e8
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-03-01 18:12:43 -0300

    Personalizando o retorno da validação

--- src/main/java/com/example/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,8 @@ import com.example.cursomc.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,5 +27,18 @@ public class ResourceExceptionHandler {
 		
 		StandarError err = new StandarError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandarError> Validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+		
+        for(FieldError x : e.getBindingResult().getFieldErrors()){
+            err.addError(x.getField(), x.getDefaultMessage());
+        }
+        
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
---
+++ src/main/java/com/example/cursomc/resources/exceptions/ValidationError.java
@@ -0,0 +1,24 @@
+package com.example.cursomc.resources.exceptions;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ValidationError extends StandarError{
+    public static long serialVersionUID=1L;
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public  ValidationError(Integer status,String msg,Long timeStamp){
+        super(status, msg, timeStamp);
+    }
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldName, String message){
+        errors.add(new FieldMessage(fieldName, message));
+    }
+
+    
+}
